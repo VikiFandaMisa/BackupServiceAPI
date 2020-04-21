6fc36be794b15b0d58b56b5a5c3923c45b2f42ff
Michal Černý <cernyymichal@gmail.com>
2020-04-21 15:10:24 +0200

    add ComputerRegistratiron annotation

--- Models/ComputerRegistration.cs
+++ Models/ComputerRegistration.cs
@@ -1,7 +1,8 @@
-using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BackupServiceAPI.Models {
+    [NotMapped]
     public class ComputerRegistration {
         [MaxLength(256)]
         public string Hostname {get; set;}
