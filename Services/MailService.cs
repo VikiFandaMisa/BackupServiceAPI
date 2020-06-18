523646dee5c0f26147f1fca8109cde0f6e832763
Victor Smith <victor.wifi1@gmail.com>
2020-06-18 22:51:24 +0200

    added table mails

--- Services/MailService.cs
+++ Services/MailService.cs
@@ -10,15 +10,18 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
-namespace BackupServiceAPI.Services {
-    public class MailService : BackgroundService {
+namespace BackupServiceAPI.Services
+{
+    public class MailService : BackgroundService
+    {
         private readonly SmtpClient _SmtpClient = new SmtpClient();
         private DbBackupServiceContext _Context;
         private readonly IServiceScopeFactory _ScopeFactory;
         private readonly IConfiguration _Configuration;
         public string Email { get; set; }
 
-        public MailService(IServiceScopeFactory scopeFactory, IConfiguration configuration) {
+        public MailService(IServiceScopeFactory scopeFactory, IConfiguration configuration)
+        {
             _ScopeFactory = scopeFactory;
             _Configuration = configuration;
 
@@ -28,22 +31,27 @@ namespace BackupServiceAPI.Services {
             _SmtpClient.Credentials = new NetworkCredential(_Configuration["SMTP:Email"], _Configuration["SMTP:Password"]);
             _SmtpClient.EnableSsl = true;
         }
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
             new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromDays(1));
         }
-        private void DoWork(object state) {
+        private void DoWork(object state)
+        {
             using var scope = _ScopeFactory.CreateScope();
             _Context = scope.ServiceProvider.GetRequiredService<DbBackupServiceContext>();
             var toSend = WriteMail();
             var accounts = new List<string>();
-            foreach (var account in scope.ServiceProvider.GetRequiredService<DbBackupServiceContext>().Accounts.ToList()) {
+            foreach (var account in scope.ServiceProvider.GetRequiredService<DbBackupServiceContext>().Accounts.ToList())
+            {
                 if (account.Admin)
                     toSend.To.Add(account.Email);
             }
             _SmtpClient.Send(toSend);
         }
-        public MailMessage WriteMail() {
-            var mailMessage = new MailMessage {
+        public MailMessage WriteMail()
+        {
+            var mailMessage = new MailMessage
+            {
                 From = new MailAddress(Email),
                 Subject = "Report for today: " + DateTime.Now,
                 Body = GetHtmlBody(),
@@ -52,43 +60,56 @@ namespace BackupServiceAPI.Services {
             return mailMessage;
         }
 
-        public string GetHtmlBody() {
+        public string GetHtmlBody()
+        {
             var now = DateTime.Now;
 
             var body = "";
 
             body += "<h1>Good day sir</h1> <h2  > Report for today " + now + "</h2><br><h3>reports:</h3><br>";
+            body += "<html><head><style>table {  font-family: arial, sans-serif;  border-collapse: collapse;  width: 100%;}td, th {  border: 1px solid #dddddd;  text-align: left;  padding: 8px;}  th{color:white; background-color: #378BC8 } ;</style></head>";
+            body += "<body><h2>Reports:</h2><table>  <tr>    <th>Client</th>    <th>Template</th>    <th>Log message</th>    <th>Date of log</th>  </tr>";
+            foreach (var p in GetLogs())
+            {
+                body += @"<tr>" + "<td>" + GetHostname(Convert.ToInt32(p.JobID)) + "</td>" + "<td>" + GetTemplateName(Convert.ToInt32(p.JobID)) + "</td>" + "<td>" + p.Message + "</td>" + "<td>" + p.Date.Date;
+            }
+            body += "</table>";
 
-            foreach (var p in GetLogs()) {
-                body += @"Client: " + GetHostname(Convert.ToInt32(p.JobID)) + "&#160&#160&#160 " + "   Template: " + GetTemplateName(Convert.ToInt32(p.JobID)) + " &#160 &#160 &#160  Log message: " + p.Message + " &#160 &#160 &#160  Date of log: " + p.Date.Date + "<br><br>";
+
+            body += "<h2>DeadClients</h2><table>  <tr>    <th>Dead Clients</th>   </tr>";
+
+            foreach (var c in GetDeadComputers())
+            {
+                body += "<tr><td>" + c.Hostname + "</td></tr>";
+            }
+            body += "</table>";
+
+            body += "<h2>NewClients</h2><table>  <tr>    <th>Newy Clients</th>   </tr>";
+
+            foreach (var c in GetComputers())
+            {
+
+                body += "<tr><td>" + c.Hostname + "</td></tr>";
             }
 
-            body += " <br><h3>DeadClients:</h3><br>";
-
-            foreach (var c in GetDeadComputers()) {
-                body += "Klient " + c.Hostname + "is Dead <br>";
-            }
-
-            body += " <br><h3>NewClients:</h3><br>";
-
-            foreach (var c in GetComputers()) {
-
-                body += "Client: " + c.Hostname + "<br>";
-            }
+            body += "</table></body></html>";
 
             return body;
 
 
         }
-        private Computer[] GetComputers() {
+        private Computer[] GetComputers()
+        {
             return _Context.Computers.ToArray();
         }
 
-        private LogItem[] GetLogs() {
+        private LogItem[] GetLogs()
+        {
             return _Context.Log.ToArray();
         }
 
-        private Computer[] GetDeadComputers() {
+        private Computer[] GetDeadComputers()
+        {
             return _Context.Computers.FromSqlRaw(@"
                 SELECT *
                 FROM Computers c                 
@@ -102,7 +123,8 @@ namespace BackupServiceAPI.Services {
 
 
 
-        private string GetTemplateName(int jobID) {
+        private string GetTemplateName(int jobID)
+        {
 
             var a = _Context.Jobs.FromSqlRaw(@"
                 SELECT *
@@ -119,7 +141,8 @@ namespace BackupServiceAPI.Services {
             return b[0].Name;
         }
 
-        private string GetHostname(int jobID) {
+        private string GetHostname(int jobID)
+        {
             var a = _Context.Jobs.FromSqlRaw(@"
                 SELECT *
                 FROM Jobs p
